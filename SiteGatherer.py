88b498e7bb82e3e335a2f57b0ec2f0681f820d2b
Ben Dodge <bensdodge255@gmail.com>
2022-03-27 17:01:19 -0400

    Add beta category gatherer

--- SiteGatherer.py
+++ SiteGatherer.py
@@ -4,8 +4,11 @@ import time
 import urllib.request
 import xml.etree.ElementTree as ET
 
+# For official use, use Full Site Ranking
+sheet_name = "bigtest"
+
 gc = gspread.service_account()
-sh = gc.open("Full Site Ranking")
+sh = gc.open(sheet_name)
 
 
 def writeSite(url):
@@ -20,13 +23,26 @@ def writeSite(url):
                 'TEXT'), xml[0][3].attrib.get('CODE'), xml[0][3].attrib.get('RANK')])
     time.sleep(3)
 
+def writeCategory(url):
+    site = urllib.request.urlopen("https://website-categorization.whoisxmlapi.com/api/v2?apiKey=at_2P50aD7BIrirqswZhTcgUC8CFrXbi&domainName=" + url)
+    tree = ET.parse(site)
+    xml = tree.getroot()
+    if(xml[2] is 1):
+        sh.sheet1.append_row(url, xml[0][0].attrib.get('name'))
+    else:
+        sh.sheet1.append_row(url, "false")
+    time.sleep(3)
 
-def readCsv(file):
+def readCsv(file, site):
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             print(row[1])
-            writeSite(row[1])
+            if(site):
+                writeSite(row[1])
+            else:
+                writeCategory(row[0])
+
 
 
 readCsv('top-1m.csv')
