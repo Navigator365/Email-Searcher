d1f9e6445f35c2497d85240ffa74beeea3c4eb7f
Ben Dodge <bensdodge255@gmail.com>
2022-03-28 18:40:42 -0400

    add functional category surfing

--- SiteGatherer.py
+++ SiteGatherer.py
@@ -28,12 +28,14 @@ def writeSite(url):
 def writeCategory(url):
     client = Client('at_2P50aD7BIrirqswZhTcgUC8CFrXbi')
     response = client.data('whoisxmlapi.com')
-    counter = 0
+    print(response)
+    count = 0
     if response.website_responded:
         for cat in response.categories:
-            if cat.tier1 and counter < 1:
-                sh.sheet1.append_row([url, cat.tier1.name])
-                counter += 1
+            if cat.tier1:
+                if(str(cat.tier1.name) != "Not enough content" and count < 1):
+                    sh.sheet1.append_row([url, str(cat.tier1.name)])
+                    count += 1
     else:
         sh.sheet1.append_row([url, "No Data"])
 
